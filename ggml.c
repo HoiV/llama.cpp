5134e8d397ede2d9ef8f05f10b953eb8da4d3c51
Hoi Vo <hoiv@microsoft.com>
2024-10-14 13:27:59 -0700

    Fine tune stats output for each prompt run Remove a verbose printf for computing blocking factor

--- ggml.c
+++ ggml.c
@@ -14847,10 +14847,10 @@ IQK_MulMat_Not_Available2:;
 //    blck0_factor = (cache_size - src0_row_size) / row_size; 
     blck0_factor = cache_size / row_size; 
     if (!blck0_factor || (blck0_factor == 1)) {
-        printf("blck factor 0/1 - cache_size %zd, src0 row size %zd, src1 row size %zd\n",
-               cache_size,
-               src0_row_size,
-               row_size);
+        //printf("blck factor 0/1 - cache_size %zd, src0 row size %zd, src1 row size %zd\n",
+        //       cache_size,
+        //       src0_row_size,
+        //       row_size);
     }
 
     //
